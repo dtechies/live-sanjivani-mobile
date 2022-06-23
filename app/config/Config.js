70950e2044f030418b8cea9224d058530e64db14
Bhrugu Tunedeliya <70563673+bhrugu09@users.noreply.github.com>
2022-06-23 15:28:43 +0530

    changed baseurl

--- app/config/Config.js
+++ app/config/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BASE_URL: 'https://29c6-2405-201-2006-7803-48a0-e014-54ba-846a.ngrok.io',
+  BASE_URL: 'https://f17a-2405-201-2006-7a06-506a-7154-d18f-e61.ngrok.io',
   // BASE_URL: 'https://38aa-2405-201-2006-7803-f47a-b42e-c20b-dd7.in.ngrok.io',
   GOOGLE_API_KEY: {
     // key: 'AIzaSyDjtilqk6uyj1gDV1lEdyhuFUu9mwobOSw',
