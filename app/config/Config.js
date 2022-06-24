54c597ddddd5c8b4b4deaafad54bc9e68a63ec0d
bhavya_savaliya <96612425+Bhavyasavaliya@users.noreply.github.com>
2022-06-24 15:15:55 +0530

    changed base url

--- app/config/Config.js
+++ app/config/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BASE_URL: 'https://9db8-2405-201-2006-7a06-b1e8-b2f9-903f-333d.in.ngrok.io',
+  BASE_URL: 'http://api.livesanjivani.com',
   // BASE_URL: 'https://38aa-2405-201-2006-7803-f47a-b42e-c20b-dd7.in.ngrok.io',
   GOOGLE_API_KEY: {
     // key: 'AIzaSyDjtilqk6uyj1gDV1lEdyhuFUu9mwobOSw',
