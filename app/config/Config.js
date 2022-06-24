8364df0e274c635508d55c9cc9904e26fc686436
Pragnesh <30809418+pragneshpj@users.noreply.github.com>
2022-06-24 18:51:23 +0530

    Update Config.js

--- app/config/Config.js
+++ app/config/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BASE_URL: 'http://api.livesanjivani.com',
+  BASE_URL: 'https://api.livesanjivani.com',
   // BASE_URL: 'https://38aa-2405-201-2006-7803-f47a-b42e-c20b-dd7.in.ngrok.io',
   GOOGLE_API_KEY: {
     // key: 'AIzaSyDjtilqk6uyj1gDV1lEdyhuFUu9mwobOSw',
